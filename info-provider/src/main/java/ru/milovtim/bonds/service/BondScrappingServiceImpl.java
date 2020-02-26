f0d154135d35acb03afa4879dbc8105ec020e707
Timur Milovanov <milovtim@ya.ru>
2020-02-26 23:21:39 +0300

    Setup jupiter test

--- info-provider/src/main/java/ru/milovtim/bonds/service/BondScrappingServiceImpl.java
+++ info-provider/src/main/java/ru/milovtim/bonds/service/BondScrappingServiceImpl.java
@@ -3,6 +3,7 @@ package ru.milovtim.bonds.service;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,15 +11,18 @@ import io.reactivex.Observable;
 import okhttp3.ResponseBody;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import ru.milovtim.bonds.module.ThirdPartyConfig;
 import ru.milovtim.bonds.pojo.BondPaymentSchedule;
 
 public class BondScrappingServiceImpl implements BondScrappingService {
 
     private final SmartLabHttpClient httpClient;
+    private final ThirdPartyConfig config;
 
     @Inject
-    public BondScrappingServiceImpl(SmartLabHttpClient httpClient) {
+    public BondScrappingServiceImpl(SmartLabHttpClient httpClient, ThirdPartyConfig config) {
         this.httpClient = httpClient;
+        this.config = config;
     }
 
     @Override
@@ -26,17 +30,28 @@ public class BondScrappingServiceImpl implements BondScrappingService {
 
     }
 
+
+    private Document parseSmartlabResponse(InputStream inputStream) {
+        try {
+            return Jsoup.parse(inputStream, "UTF-8", config.getSmartlabHost());
+        } catch (IOException e) {
+            throw new RuntimeException("Error parsing http response body", e);
+        }
+    }
+
+    //doc = Jsoup.connect("https://smart-lab.ru/q/bonds/RU000A0JWXQ7/").get();
     @Override
     public BondPaymentSchedule getBondSchedule(String isin) {
-        Observable<ResponseBody> bondPageObs = httpClient.getBondPage(isin);
-        Observable<Document> docObs = bondPageObs.map(responseBody -> Jsoup.parse(responseBody.byteStream(), "UTF-8", null));
-//        doc = Jsoup.connect("https://smart-lab.ru/q/bonds/RU000A0JWXQ7/").get();
-        List<BondPaymentSchedule.BondPayment> coupons = docObs.map(doc ->
-            doc.select(".simple-little-table.bond tbody tr").stream()
-            .skip(1)//skip table header
-            .map(element -> new BondPaymentSchedule.BondPayment())
-            .collect(Collectors.toList())
-        ).blockingFirst();
+        List<BondPaymentSchedule.BondPayment> coupons =
+            httpClient.getBondPage(isin)
+                .map(ResponseBody::byteStream)
+                .map(this::parseSmartlabResponse)
+                .map(doc -> doc.select(".simple-little-table.bond tbody").eq(1).select("tr").stream()
+                    .skip(1)//skip table header
+                    //todo fill POJO with data from html table
+                    .map(element -> new BondPaymentSchedule.BondPayment())
+                    .collect(Collectors.toList())
+                ).blockingFirst();
 
         BondPaymentSchedule schedule = new BondPaymentSchedule();
         schedule.setPayments(coupons);
--- info-provider/src/test/java/ru/milovtim/bonds/service/BondScrappingServiceImplTest.java
+++ info-provider/src/test/java/ru/milovtim/bonds/service/BondScrappingServiceImplTest.java
@@ -1,19 +1,58 @@
 package ru.milovtim.bonds.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.google.common.io.ByteStreams;
+import io.reactivex.Observable;
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
+import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.milovtim.bonds.module.ThirdPartyConfig;
+import ru.milovtim.bonds.pojo.BondPaymentSchedule;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class BondScrappingServiceImplTest {
 
     BondScrappingServiceImpl service;
 
+    @Mock SmartLabHttpClient client;
+    @Mock ThirdPartyConfig config;
+
     @BeforeEach
     void setUp() {
-        service = new BondScrappingServiceImpl(null);
+        service = new BondScrappingServiceImpl(client, config);
     }
 
     @Test
-    void getBondSchedule() {
-        
+    void getBondSchedule() throws Exception {
+        when(config.getSmartlabHost()).thenReturn("http://hello.com");
+        when(client.getBondPage(anyString())).thenReturn(Observable.just(getRespBodyFromFile()));
+        BondPaymentSchedule schedule = service.getBondSchedule("hello");
+        assertNotNull(schedule);
+    }
+
+    @NotNull
+    private ResponseBody getRespBodyFromFile() throws IOException {
+        InputStream httpRespInFile = getClass().getResourceAsStream("/smartlab-response.html");
+        return ResponseBody.create(MediaType.get("text/html"), ByteStreams.toByteArray(httpRespInFile));
+    }
+
+    @Test
+    void playWithJsoup() throws Exception {
+        Document doc = Jsoup.parse(getRespBodyFromFile().byteStream(), UTF_8.name(), "https://smart-lab.ru");
+ (th       assertNotNull(doc);
     }
 }
