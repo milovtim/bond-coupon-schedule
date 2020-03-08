9ce57ee07a7ee8d528a5a1d6f687d0247e4d0e47
Timur Milovanov <milovtim@ya.ru>
2020-03-08 13:59:13 +0300

    Basic parser

--- info-provider/src/main/java/ru/milovtim/bonds/Test.java
+++ info-provider/src/main/java/ru/milovtim/bonds/Test.java
@@ -1,17 +1,34 @@
 package ru.milovtim.bonds;
 
-import com.google.common.base.Preconditions;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import ru.milovtim.bonds.module.TinkoffApiClientModule;
+import org.apache.commons.cli.*;
 import ru.milovtim.bonds.module.MainModule;
+import ru.milovtim.bonds.module.TinkoffApiClientModule;
 import ru.milovtim.bonds.pojo.BondPaymentSchedule;
 import ru.milovtim.bonds.service.BondScrappingService;
 
 public class Test {
+    static final CommandLineParser CLI_PARSER;
+    static final Options CLI_PARSER_OPTS;
+    static {
+        CLI_PARSER = new DefaultParser();
+        CLI_PARSER_OPTS = new Options()
+            .addOption(Option.builder("t")
+                .longOpt("token")
+                .hasArg(true)
+                .longOpt("Tinkoff API token (generated in settings page)")
+                .required(true).build())
+            ;
+    }
+
 
     public static void main(String[] args) {
-        Preconditions.checkArgument(args.length > 0, "Token must be provided");
+        try {
+            CLI_PARSER.parse(CLI_PARSER_OPTS, args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         boolean sandbox = (args.length > 1) && Boolean.parseBoolean(args[1]);
 
         Injector injector = Guice.createInjector(new TinkoffApiClientModule(args[0], sandbox), new MainModule());
